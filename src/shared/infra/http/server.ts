eedb8460b0bac6781e736213173718b92598ef58
Guilherme Maciel <guilherme.maciel.9889@gmail.com>
2022-04-06 09:00:35 -0300

    Adição do babel no projeto

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,23 +1,25 @@
+import "reflect-metadata";
+import "dotenv/config";
+import cors from "cors";
 import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
 import swaggerUI from "swagger-ui-express";
 
-import "../typeorm";
-import { router } from "@shared/infra/http/routes/index";
-import "../../container";
-
+import "@shared/container";
 import { AppError } from "@shared/errors/AppError";
+import createConnection from "@shared/infra/typeorm";
 
 import swaggerFile from "../../../swagger.json";
+import { router } from "./routes";
+
+createConnection();
 
 const app = express();
-
-app.use(express.json());
-
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
+app.use(express.json());
+app.use(cors());
 
 app.use(router);
-
 app.use(
     (err: Error, request: Request, response: Response, next: NextFunction) => {
         if (err instanceof AppError) {
